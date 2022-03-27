0c2a9942dc708a319cdf175e4873d974740081eb
udaycruise2903 <56758575+udaycruise2903@users.noreply.github.com>
2022-03-27 14:40:01 +0530

    commented breadcrumb code in foundation.hs

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -100,13 +100,13 @@ instance Yesod App where
     defaultLayout :: Widget -> Handler Html
     defaultLayout widget = do
         master <- getYesod
-        mmsg <- getMessage
+--        mmsg <- getMessage
 
         muser <- maybeAuthPair
         mcurrentRoute <- getCurrentRoute
 
         -- Get the breadcrumbs, as defined in the YesodBreadcrumbs instance.
-        (title, parents) <- breadcrumbs
+--        (title, parents) <- breadcrumbs
 
         -- Define the menu items of the header.
         let menuItems =
